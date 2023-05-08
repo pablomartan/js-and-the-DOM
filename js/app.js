5ac942c9dcb6fa616f89fcd38238a8f85eecb389
pablomartan <pablomartan@kolabnow.com>
2023-05-08 18:15:07 +0200

    refactor: complete refactor of all functions

--- js/app.js
+++ js/app.js
@@ -1,14 +1,8 @@
 /*
- * Desired functionality:
- *  - [x] Scroll to section
- *  - [x] Scroll back to top
- *  - [x] Populate navigation bar
- *  - [ ] Collapse section
+ *
+ * FUNCTIONS
+ *
  */
-
-const backToTopButton = document.getElementById('back-to-top');
-const navBar = document.getElementById('nav-bar');
-const sectionCollection = Array.from(document.getElementsByTagName('section'));
 
 /**
  * @description: scrolls to a given section
@@ -24,17 +18,11 @@ const scrollToSection = sectionId => {
   });
 };
 
-const sections = Array.from(document.getElementsByTagName('section'));
-sections.forEach(section => section.addEventListener('click', e => {
-  scrollToSection(section.id);
-}));
-
-
 /**
  * @description: create a list item with a link to the given section
  * @param {HTMLElement} section: the section to link to
  */
-const createLi = section => {
+const createNavItem = section => {
   const p = document.createElement('p');
   p.innerText = section.dataset.sectionName
   p.classList.add('nav-item');
@@ -43,22 +31,23 @@ const createLi = section => {
 };
 
 /**
- * @description: create a list item for each section of the page, with a link
- * to said section
+ * @description: create a list item for each section of the page
  * @param {HTMLCollection} sections: the section collection
  */
 const buildNavBar = sections => {
+  const navBar = document.getElementById('nav-bar');
   const fragment = new DocumentFragment();
 
-  sections.forEach(section => {
-    fragment.appendChild(createLi(section));
-  });
+  for (let section of sections) {
+    fragment.appendChild(createNavItem(section));
+  }
 
   navBar.appendChild(fragment);
 };
 
-/*
- * Collapse sections
+/**
+ * @description: collapses and expands sections on demand
+ * @param {Event} e: the 'click' event which will trigger the function
  */
 const collapse = e => {
   const section = e.target.parentElement;
@@ -68,70 +57,113 @@ const collapse = e => {
   parag.classList.toggle('hidden');
 };
 
-/*
- * Show scroll back button when bottom of the page is reached
+/**
+ * @description: shows the navigation bar given a certain conditions
+ * @param {Event} e: the event that triggered the function; one of:
+ *  [scroll, pointerup, click, mousemove]
  */
-document.addEventListener('scroll', () => {
-  (window.innerHeight + Math.round(window.scrollY)) >= document.body.scrollHeight
-    ? backToTopButton.classList.remove('hidden')
-    : backToTopButton.classList.add('hidden');
-});
+const showNavBar = e => {
+  const navBar = document.getElementById('nav-bar');
 
-/*
- * Show navbar when:
- *  - user scrolls
- *  - user clicks/taps top of the page
- *  - user places mouse on top of the page
- */
-let timeOut = null;
+  // handle scroll event, and show only if not in top of the page
+  e.type === 'scroll' && window.scrollY > 0 && navBar.classList.remove('hidden');
 
-window.addEventListener('scroll', () => {
-  const classArray = Array.from(navBar.classList);
-  if (window.scrollY > 0 && classArray.includes('hidden')) {
-    navBar.classList.remove('hidden');
-  }
+  /*
+   * handle pointer related events, and show only if they happen on the top 70
+   * pixels
+   */
+  e.clientY <= 70 && navBar.classList.remove('hidden');
 
   window.clearTimeout(timeOut);
-
   timeOut = setTimeout(() => navBar.classList.add('hidden'), 1500);
-});
+}
 
-const handlePointerUp = e => {
-  if (e.clientY <= 70) {
-    navBar.classList.remove('hidden');
-    window.clearTimeout(timeOut);
-    timeOut = setTimeout(() => navBar.classList.add('hidden'), 1500);
-  }
-};
+/**
+ * @description: sets active class for nav-item when correct section is on
+ * viewport
+ */
+const setActiveNavItem = () => {
+  const sections = document.getElementsByTagName('section');
+  const navItems = document.getElementsByClassName('nav-item');
 
-document.onpointerup = handlePointerUp;
-document.onclick = handlePointerUp;
-document.onmousemove = handlePointerUp;
-
-const setActiveNavItem = e => {
-  sectionCollection.forEach((section, i) => {
+  for (let i = 0; i < sections.length; i++) {
+    const [section, navItem] = [sections[i], navItems[i]];
     const coords = section.getBoundingClientRect();
     const topHalfInView = coords.top > 0 && coords.top <= window.innerHeight / 2;
     const bottomHalfInView = coords.bottom <= window.innerHeight && coords.bottom >= window.innerHeight / 2;
-    const navItems = Array.from(document.getElementsByClassName('nav-item'));
 
-    section.classList.contains('collapsed') ?
-      topHalfInView && bottomHalfInView ? navItems[i].classList.add('active')
-        : navItems[i].classList.remove('active')
-      : topHalfInView || bottomHalfInView ? navItems[i].classList.add('active') : navItems[i].classList.remove('active');
-  });
+    // unset active class if previously set
+    navItem.classList.remove('active');
+
+    /*
+     * if section is collapsed, only set active when both top and bottom of
+     * section are on viewport
+    */
+    section.classList.contains('collapsed') && topHalfInView && bottomHalfInView && navItem.classList.add('active');
+
+    /*
+     * if section is not collapsed, then set active class if either top half of
+     * section is on top half of viewport, or bottom half of section is on
+     * bottom half of viewport
+     */
+    (topHalfInView || bottomHalfInView) && navItem.classList.add('active');
+  }
 };
 
-document.addEventListener('scroll', setActiveNavItem);
+/**
+ * @description: set visibility of #back-to-top button
+ */
+const toggleBackToTopButton = () => {
+  const button = document.getElementById('back-to-top');
+  
+  (window.innerHeight + Math.round(window.scrollY)) >= document.body.scrollHeight
+    ? button.classList.remove('hidden')
+    : button.classList.add('hidden');
+};
 
-backToTopButton.addEventListener('click', () => window.scroll({
+/*
+ *
+ * EVENTS
+ *
+ */
+
+/*
+ * Show scroll back button when bottom of the page is reached
+ */
+document.addEventListener('scroll', toggleBackToTopButton);
+
+/*
+ * Add ability to scroll back to top of the page to #back-to-top button
+ */
+document.getElementById('back-to-top').addEventListener('click', () => window.scroll({
   top: 0,
   left: 0,
   behavior: 'smooth'
 }));
 
-document.addEventListener('DOMContentLoaded', buildNavBar(sectionCollection));
+/*
+ * Show navigation bar when scrolling, when clicking on top of the screen, or
+ * when mouseover the top of the screen
+ */
+let timeOut = null;
+document.addEventListener('scroll', showNavBar);
+document.onpointerup = showNavBar;
+document.onclick = showNavBar;
+document.onmousemove = showNavBar;
 
-Array.from(document.getElementsByTagName('H2')).forEach(title => {
+/*
+ * Set active nav-item
+ */
+document.addEventListener('scroll', setActiveNavItem);
+
+/*
+ * Build the navigation bar when the DOM has loaded
+ */
+document.addEventListener('DOMContentLoaded', buildNavBar(document.getElementsByTagName('section')));
+
+/*
+ * Add click event to section headers
+ */
+for (let title of document.getElementsByTagName('h2')) {
   title.addEventListener('click', collapse);
-});
+}
